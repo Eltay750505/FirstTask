b7b04797ab01ab315fd834e8358a96fc4de9e035
Eltay <romanov1234567890987@mail.ru>
2021-04-01 18:17:14 +0300

    added new soluion

--- src/main/java/ru/gmail/gasimov/firsttask/parse/CustomParser.java
+++ src/main/java/ru/gmail/gasimov/firsttask/parse/CustomParser.java
@@ -3,9 +3,10 @@ package ru.gmail.gasimov.firsttask.parse;
 import java.util.Arrays;
 
 public class CustomParser {
+    public static final String SEPARATOR = " ";
     public int[] parseStringIntoIntArray(String string) {
         int[] numbers = Arrays
-                .stream(string.split(" "))
+                .stream(string.split(SEPARATOR))
                 .mapToInt(Integer::parseInt)
                 .toArray();
         return numbers;
